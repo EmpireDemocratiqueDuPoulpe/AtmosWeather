0a33b7f67b14f879d01811f145f0a18b62eba89b
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-04-16 17:56:03 +0200

    Added React Router

---
+++ src/pages/App/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+import {Route, Router} from "react-router";
+import { createBrowserHistory } from "history";
+import Index from "../Index/Index.js";
+
+const browserHistory = createBrowserHistory();
+
+export default function App() {
+	return (
+		<Router history={browserHistory}>
+			<main>
+				<header className="App-header"> </header>
+
+				<div className="App-body">
+					<Route exact path="/" component={Index}/>
+				</div>
+			</main>
+		</Router>
+	);
+}
