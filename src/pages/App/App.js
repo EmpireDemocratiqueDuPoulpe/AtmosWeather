5ed1cad4156b21b052960d0475f9335bbb84b40a
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-04-27 17:24:20 +0200

    Fixed router and redirect bug

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,31 +1,29 @@
 import React from "react";
-import { Router, Route } from "react-router";
-import { Link } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Themes from "../../components/Themes/Themes.js";
 import GlobalStyles from "../../components/Themes/GlobalStyles/GlobalStyles.js";
 import useThemes from "../../components/Themes/useThemes.js";
 import ThemeSwitcher from "../../components/Themes/ThemeSwitcher/ThemeSwitcher.js";
 import useToken from "../../components/Auth/useToken.js";
+import useUID from "../../components/Auth/useUID.js";
 import Index from "../Index/Index.js";
 import Register from "../Register/Register.js";
 import LogIn from "../LogIn/LogIn.js";
 import LogOut from "../LogOut/LogOut.js";
+import Errors from "../Errors/Errors.js";
 import "./App.css";
 
-const browserHistory = createBrowserHistory();
-
-// TODO: Fix <Redirect/> bug
 export default function App() {
 	const [ theme, toggleTheme, ready ] = useThemes();
 	const currentTheme = (theme === "light" ? Themes.light : Themes.dark);
 	const { token } = useToken();
+	const { uid } = useUID();
 
 	if (!ready) return <div/>;
 
 	return (
-		<Router history={browserHistory}>
+		<Router>
 			<ThemeProvider theme={currentTheme}>
 				<GlobalStyles/>
 
@@ -36,10 +34,15 @@ export default function App() {
 					</header>
 
 					<div className="App-body">
-						<Route exact path={["/", "//", "/index"]} component={Index}/>
-						<Route path="/register" component={Register}/>
-						<Route path="/login" component={LogIn}/>
-						<Route path="/logout" component={LogOut}/>
+						<Switch>
+							<Route exact path={["/", "//", "/index"]}>
+								<Index uid={uid}/>
+							</Route>
+							<Route path="/register" component={Register}/>
+							<Route path="/login" component={LogIn}/>
+							<Route path="/logout" component={LogOut}/>
+							<Route component={Errors.NotFound}/>
+						</Switch>
 					</div>
 				</main>
 			</ThemeProvider>
