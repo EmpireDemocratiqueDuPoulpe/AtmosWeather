b9ea0b0da275729cef7fd20ec946f44d16753533
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-03 11:45:57 +0200

    Added style to Errors page

--- src/pages/Errors/NotFound/NotFound.js
+++ src/pages/Errors/NotFound/NotFound.js
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
 	return (
-		<div>
-			<h2>Page inexistante</h2>
-			<Link to="/">Retour à la page principale</Link>
+		<div className="error-box error-404">
+			<h2 className="error-code highlight-font">404</h2>
+			<h3 className="error-message highlight-font">Page inexistante</h3>
+			<h4 className="error-emoji highlight-font">( ͠° ʖ̯ ͡°)</h4>
+			<Link className="error-link link red" to="/">Retour à la page principale</Link>
 		</div>
 	);
 }
